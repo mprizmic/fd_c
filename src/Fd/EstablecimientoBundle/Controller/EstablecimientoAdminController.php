4da9916b625fe0c10e545673a26dd664e79b9247
marcelo <mprizmic@buenosaires.gob.ar>
2014-05-06 11:20:19 -0300

    arranque. puesta a punto

--- src/Fd/EstablecimientoBundle/Controller/EstablecimientoAdminController.php
+++ src/Fd/EstablecimientoBundle/Controller/EstablecimientoAdminController.php
@@ -4,5 +4,5 @@ namespace Fd\EstablecimientoBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
 
-class AdminController extends Controller {
+class EstablecimientoAdminController extends Controller {
 }
