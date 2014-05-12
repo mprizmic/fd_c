0093aeb3702a819529eaf356f4fe39999f079d45
marcelo <mprizmic@buenosaires.gob.ar>
2014-05-12 16:40:25 -0300

    se agregan validadores

---
+++ src/Fd/EstablecimientoBundle/Validator/Constraints/ApellidoValidator.php
@@ -0,0 +1,15 @@
+<?php
+namespace Fd\EstablecimientoBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ApellidoValidator extends ConstraintValidator
+{
+    public function validate( $value, Constraint $constraint)
+    {
+        if (0 === preg_match('/^[a-zA-Z \-\'üÜñÑ]+$/',$value)){
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
