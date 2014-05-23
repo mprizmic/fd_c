cdeacc84c04f80b4a763b624c978c47a01c5e0fb
marcelo <mprizmic@buenosaires.gob.ar>
2014-05-23 11:18:45 -0300

    falto el agregado a la portada

---
+++ src/Fd/BackendBundle/Resources/doc/portada.sql
@@ -0,0 +1,14 @@
+INSERT INTO `Fd`.`portada`
+(
+`tabla`,
+`descripcion`,
+`actualizado`,
+`creado`,
+`url`)
+VALUES
+(
+"OrigenHora",
+"Si un lab/centro se financia por POF u horas cátedra",
+"2014-05-23 10:10:10",
+"2014-05-23 10:10:10",
+"nada");
