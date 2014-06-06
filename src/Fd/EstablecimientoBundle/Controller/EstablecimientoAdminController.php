ec0f1ff266b527098bdd04899676a35c57081271
marcelo <mprizmic@buenosaires.gob.ar>
2014-06-06 13:14:30 -0300

    arreglando

--- src/Fd/EstablecimientoBundle/Controller/EstablecimientoAdminController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Fd\EstablecimientoBundle\Controller;
-
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
-
-class EstablecimientoAdminController extends Controller {
-}
