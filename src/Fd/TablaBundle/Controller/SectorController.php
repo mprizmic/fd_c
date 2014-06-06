474059dd399e72760b911969049926de6c96e025
marcelo <mprizmic@buenosaires.gob.ar>
2014-06-06 15:20:00 -0300

    crud de sector hecho con jordillonch y arreglado con bootstrap

---
+++ src/Fd/TablaBundle/Controller/SectorController.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace Fd\TablaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrapView;
+
+use Fd\TablaBundle\Entity\Sector;
+use Fd\TablaBundle\Form\SectorType;
+use Fd\TablaBundle\Form\SectorFilterType;
+
+/**
+ * Sector controller.
+ *
+ * @Route("/sector")
+ */
+class SectorController extends Controller
+{
+    /**
+     * Lists all Sector entities.
+     *
+     * @Route("/", name="sector")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        list($filterForm, $queryBuilder) = $this->filter();
+
+        list($entities, $pagerHtml) = $this->paginator($queryBuilder);
+
+    
+        return array(
+            'entities' => $entities,
+            'pagerHtml' => $pagerHtml,
+            'filterForm' => $filterForm->createView(),
+        );
+    }
+
+    /**
+    * Create filter form and process filter request.
+    *
+    */
+    protected function filter()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $filterForm = $this->createForm(new SectorFilterType());
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('TablaBundle:Sector')->createQueryBuilder('e');
+    
+        // Reset filter
+        if ($request->getMethod() == 'POST' && $request->get('filter_action') == 'reset') {
+            $session->remove('SectorControllerFilter');
+        }
+    
+        // Filter action
+        if ($request->getMethod() == 'POST' && $request->get('filter_action') == 'filter') {
+            // Bind values from the request
+            $filterForm->bind($request);
+
+            if ($filterForm->isValid()) {
+                // Build the query from the given form object
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+                // Save filter to session
+                $filterData = $filterForm->getData();
+                $session->set('SectorControllerFilter', $filterData);
+            }
+        } else {
+            // Get filter from session
+            if ($session->has('SectorControllerFilter')) {
+                $filterData = $session->get('SectorControllerFilter');
+                $filterForm = $this->createForm(new SectorFilterType(), $filterData);
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+            }
+        }
+    
+        return array($filterForm, $queryBuilder);
+    }
+
+    /**
+    * Get results from paginator and get paginator view.
+    *
+    */
+    protected function paginator($queryBuilder)
+    {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $this->getRequest()->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+    
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me)
+        {
+            return $me->generateUrl('sector', array('page' => $page));
+        };
+    
+        // Paginator - view
+        $translator = $this->get('translator');
+        $view = new TwitterBootstrapView();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => $translator->trans('views.index.pagprev', array(), 'JordiLlonchCrudGeneratorBundle'),
+            'next_message' => $translator->trans('views.index.pagnext', array(), 'JordiLlonchCrudGeneratorBundle'),
+        ));
+    
+        return array($entities, $pagerHtml);
+    }
+    
+    /**
+     * Finds and displays a Sector entity.
+     *
+     * @Route("/{id}/show", name="sector_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TablaBundle:Sector')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Sector entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Sector entity.
+     *
+     * @Route("/new", name="sector_new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Sector();
+        $form   = $this->createForm(new SectorType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Sector entity.
+     *
+     * @Route("/create", name="sector_create")
+     * @Method("post")
+     * @Template("TablaBundle:Sector:new.html.twig")
+     */
+    public function createAction()
+    {
+        $entity  = new Sector();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new SectorType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.create.success');
+
+            return $this->redirect($this->generateUrl('sector_show', array('id' => $entity->getId())));        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.create.error');
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+    /**
+     * Displays a form to edit an existing Sector entity.
+     *
+     * @Route("/{id}/edit", name="sector_edit")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TablaBundle:Sector')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Sector entity.');
+        }
+
+        $editForm = $this->createForm(new SectorType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Sector entity.
+     *
+     * @Route("/{id}/update", name="sector_update")
+     * @Method("post")
+     * @Template("TablaBundle:Sector:edit.html.twig")
+     */
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TablaBundle:Sector')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Sector entity.');
+        }
+
+        $editForm   = $this->createForm(new SectorType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        $request = $this->getRequest();
+
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.update.success');
+
+            return $this->redirect($this->generateUrl('sector_edit', array('id' => $id)));
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.update.error');
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Sector entity.
+     *
+     * @Route("/{id}/delete", name="sector_delete")
+     * @Method("post")
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('TablaBundle:Sector')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Sector entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
+        }
+
+        return $this->redirect($this->generateUrl('sector'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
