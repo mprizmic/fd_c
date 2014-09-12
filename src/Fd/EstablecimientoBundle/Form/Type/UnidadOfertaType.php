f3f21f08ac22dff0a62069dbd441b408e5d5fb07
mprizmic <mprizmic@buenosaires.gob.ar>
2014-09-12 12:30:50 -0300

    se agrega el controlador de unidad_oferta para poner la accion que corresponde al abm de sus turnos. Se crean los type y las paginas

---
+++ src/Fd/EstablecimientoBundle/Form/Type/UnidadOfertaType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fd\EstablecimientoBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Fd\EstablecimientoBundle\Entity\UnidadOfertaTurno;
+use Fd\EstablecimientoBundle\Form\Type\UnidadOfertaTurnoType;
+
+class UnidadOfertaType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('id')
+                ->add('unidades')
+                ->add('ofertas')
+                ->add('turnos', 'collection', array(
+                    'type' => new UnidadOfertaTurnoType(),
+                    'by_reference' => FALSE,
+                    'allow_delete' => TRUE,
+                    'allow_add' => TRUE,
+                ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Fd\EstablecimientoBundle\Entity\UnidadOferta',
+        ));
+    }
+
+    public function getName() {
+        return 'fd_unidadoferta_type';
+    }
+
+}
