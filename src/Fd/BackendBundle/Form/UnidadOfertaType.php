8b39cde514f332eb942dec006973549b18d09f08
mprizmic <mprizmic@gmail.com>
2014-09-18 00:43:30 -0300

    cargar turnos a todas las ofertas de todos los niveles
    se cambia el edit de unidad oferta del backend para que se carguen
    turnos

--- src/Fd/BackendBundle/Form/UnidadOfertaType.php
+++ src/Fd/BackendBundle/Form/UnidadOfertaType.php
@@ -4,19 +4,25 @@ namespace Fd\BackendBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Fd\EstablecimientoBundle\Form\Type\UnidadOfertaTurnoType;
 
-class UnidadOfertaType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class UnidadOfertaType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('unidades')
-            ->add('ofertas')
+                ->add('unidades')
+                ->add('ofertas')
+                ->add('turnos', 'collection', array(
+                    'type' => new UnidadOfertaTurnoType(),
+                    'by_reference' => FALSE,
+                    'allow_delete' => TRUE,
+                    'allow_add' => TRUE,
+                ))
         ;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'fd_establecimientobundle_unidadofertatype';
     }
+
 }
