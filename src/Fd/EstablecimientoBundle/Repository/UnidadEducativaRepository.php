dfe78d1bc0648a062b8639bbf6ee299936cbc33b
mprizmic <mprizmic@buenosaires.gob.ar>
2014-09-19 17:50:59 -0300

    documentacion

--- src/Fd/EstablecimientoBundle/Repository/UnidadEducativaRepository.php
+++ src/Fd/EstablecimientoBundle/Repository/UnidadEducativaRepository.php
@@ -8,34 +8,6 @@ use Fd\TablaBundle\Entity\Nivel;
 
 class UnidadEducativaRepository extends EntityRepository {
 
-//    /**
-//     * Actualiza un registro de la tabla.
-//     * Para el caso en que se esté eliminando directamente la unidad educativa, los turnos se borran en cascada. 
-//     * FALTA ver que pasa con el resto de las relaciones: unidad_oferta, cohortes, etc
-//     * 
-//     * Para el caso en que se esté eliminando algún turno, hay que revisar a mano la relación con la tabla TurnoUnidadEducativa
-//     * 
-//     * @param type $entity
-//     * @return \Fd\EstablecimientoBundle\Entity\Respuesta
-//     */
-//    public function actualizar($entity) {
-//        $respuesta = new Respuesta();
-//
-//        try {
-//            $this->_em->persist($entity);
-//            $this->_em->flush();
-//            $respuesta->setClaveNueva($entity->getId());
-//
-//            $respuesta->setCodigo(1);
-//            $respuesta->setMensaje('Se guardó la unidad educativa exitosamente');
-//        } catch (Exception $e) {
-//            $respuesta->setCodigo(2);
-//            $respuesta->setMensaje('No se pudo guardar la unidad educativa. Verifique los datos y reintente');
-//        }
-//
-//        return $respuesta;
-//    }
-
     /**
      * devuelve la query para preguntar por todos los niveles de un establecimiento
      */
