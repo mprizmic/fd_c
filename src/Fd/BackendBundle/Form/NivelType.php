6c3c2898a88f174288de0bcda1c07213bb04b8cb
mprizmic <mprizmic@buenosaires.gob.ar>
2014-10-01 16:27:14 -0300

    se agrega el campo con el nombre de la clase para manejar las unidad_oferta por nivel

--- src/Fd/BackendBundle/Form/NivelType.php
+++ src/Fd/BackendBundle/Form/NivelType.php
@@ -5,19 +5,35 @@ namespace Fd\BackendBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class NivelType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class NivelType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('nombre')
-            ->add('abreviatura')
-            ->add('orden')
+                ->add('nombre', 'text', array(
+                    'attr' => array(
+                        'size' => 3,
+                    )
+                ))
+                ->add('abreviatura', 'text', array(
+                    'attr' => array(
+                        'size' => 3,
+                    )
+                ))
+                ->add('orden', 'number', array(
+                    'attr' => array(
+                        'size' => 2,
+                    )
+                ))
+                ->add('crearUOClass', 'text', array(
+                    'attr' => array(
+                        'size' => 50,
+                    )
+                ))
         ;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'fd_tablabundle_niveltype';
     }
+
 }
