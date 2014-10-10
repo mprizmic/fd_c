0ed25f7039a28f3f78de81b2731f7dd232f82541
mprizmic <mprizmic@buenosaires.gob.ar>
2014-10-10 14:22:41 -0300

    avances en el nuevo test

--- src/Fd/EstablecimientoBundle/Tests/Controller/EstablecimientoControllerTest.php
+++ src/Fd/EstablecimientoBundle/Tests/Controller/EstablecimientoControllerTest.php
@@ -6,48 +6,109 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EstablecimientoControllerTest extends WebTestCase {
 
+    private $client = null;
+    private $crawler = null;
+
+    public function setUp() {
+        $this->client = static::createClient();
+        $this->client->followRedirects(true);
+
+        $this->crawler = $this->client->request('GET', '/usuario/login');
+        $form = $this->crawler->selectButton('Remitir')->form();
+        $form['_username'] = 'marcelo';
+        $form['_password'] = '888';
+        $this->crawler = $this->client->submit($form);
+    }
+
+    public function testDocentesNivelListado() {
+        $client = $this->client;
+        $crawler = $this->crawler;
+
+        // Llama a la página del index
+        $crawler = $client->request('GET', '/establecimiento/docentes_nivel_listado');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        // Verifica que se visualizò la pagina de edicion
+        $this->assertGreaterThan(0, $crawler
+                        ->filter('html:contains("Dickens")')
+                        ->count(), 'Se visualiza la página de creación de establecimiento');
+    }
+
+    public function testCuadroMatricula() {
+
+        // llamada al recurso
+        $this->crawler = $this->client->request('GET', '/establecimiento/cuadro_matricula/14/carrera');
+        $this->assertTrue(200 === $this->client->getResponse()->getStatusCode());
+
+        $this->assertGreaterThan(0, $this->crawler
+                        ->filter('html:contains("Graduados")')
+                        ->count(), 'se visualiza la página de matrícula'
+        );
+    }
+
     public function testDamero() {
-        // Create a new client to browse the application
-        $client = static::createClient();
+        $client = $this->client;
+        $crawler = $this->crawler;
 
         // Create a new entry in the database
         $crawler = $client->request('GET', '/establecimiento/damero');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
-        $this->assertTrue($crawler->filter('html:contains("Bienvenido al Sistema de la Dirección de Formación Docente!")')->count() == 0);
+        $this->assertTrue($crawler->filter('html:contains("Bienvenido al Sistema de la Dirección de Formación Docente!")')->count() > 0);
     }
 
     public function testEstablecimientoDeUnCui() {
-        // Create a new client to browse the application
-        $client = static::createClient();
+
+        $client = $this->client;
+        $crawler = $this->crawler;
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/establecimiento_de_un_cui/21');
+        $crawler = $client->request('GET', '/establecimiento/establecimiento_de_un_cui/21');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
-        $this->assertTrue($crawler->filter('html:contains("Escuela Normal Superior Nro 8 Julio Argentino Roca")')->count() == 0);
+        $this->assertGreaterThan(
+                0, $crawler
+                        ->filter('html:contains("Distrito escolar:")')
+                        ->count());
     }
+
+    public function testFicha() {
+        $client = $this->client;
+        $crawler = $this->crawler;
+
+        // Create a new entry in the database
+        $crawler = $client->request('GET', '/establecimiento/ficha/13');
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        HASTA aca
+    }
+
+    public function testNomina() {
+        
+    }
+
+    public function testNuevoNroAnexo() {
+        
+    }
+
     public function testTarjetaEstablecimiento() {
-        // Create a new client to browse the application
-        $client = static::createClient();
+        $client = $this->client;
+        $crawler = $this->crawler;
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/tarjeta_establecimiento/21');
+        $crawler = $client->request('GET', '/establecimiento/tarjeta_establecimiento/21');
 
-        $this->assertTrue($crawler->filter('html:contains("Escuela Normal Superior Nro 8 Julio Argentino Roca")')->count() == 0);
+        $this->assertGreaterThan(0, $crawler
+                        ->filter('html:contains("CUE:")')
+                        ->count()
+        );
     }
-    public function testCuadroMatricula() {
-        // Create a new client to browse the application
-        $client = static::createClient();
 
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/establecimiento/cuadro_matricula/14/carrera');
-
-        $this->assertTrue($crawler->filter('html:contains("Cuadro de matrícula por carreras")')->count() == 0);
-    }
-    //establecimiento_de_un_cue
-    //
-    //Escuela Normal Superior Nro 8 Julio Argentino Roca
-    //
-    //
-    //ficha
-    //nuevoNroAnexo
 }
