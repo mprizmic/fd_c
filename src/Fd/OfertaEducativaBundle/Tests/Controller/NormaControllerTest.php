267a4a31823602f432b4b89b02bd068a617b06e3
mprizmic <mprizmic@buenosaires.gob.ar>
2014-10-14 12:59:00 -0300

    tests nuevos

---
+++ src/Fd/OfertaEducativaBundle/Tests/Controller/NormaControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fd\OfertaEducativaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class NormaControllerTest extends WebTestCase {
+
+    public function testNorma() {
+        
+    }
+
+    public function testBuscarAsignarCarrera() {
+        
+    }
+
+    public function testObtenerNormasPaginadas() {
+        //por ahora no se testea
+    }
+
+}
