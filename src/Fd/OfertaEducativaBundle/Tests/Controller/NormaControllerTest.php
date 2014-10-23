de1e0b6bfad7ac984841be8d67574cca1c496f08
mprizmic <mprizmic@gmail.com>
2014-10-22 22:02:20 -0300

    avance con los test

--- src/Fd/OfertaEducativaBundle/Tests/Controller/NormaControllerTest.php
+++ src/Fd/OfertaEducativaBundle/Tests/Controller/NormaControllerTest.php
@@ -2,16 +2,26 @@
 
 namespace Fd\OfertaEducativaBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Fd\EstablecimientoBundle\Tests\Controller\LoginWebTestCase;
 
-class NormaControllerTest extends WebTestCase {
-
+/**
+ * FLATA obtenernormaspaginadas
+ * Testea el frontend de norma. Es una busqueda y una página de asignación
+ */
+class NormaControllerTest extends LoginWebTestCase {
+    
+    public $x = '/oferta/norma';
+    
     public function testNorma() {
-        
+        $client = $this->client;
+        $client->request('GET', $this->x.'/ver/4');
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function testBuscarAsignarCarrera() {
-        
+        $client = $this->client;
+        $client->request('GET', $this->x.'/norma_buscar_asignar_carrera/1');
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function testObtenerNormasPaginadas() {
