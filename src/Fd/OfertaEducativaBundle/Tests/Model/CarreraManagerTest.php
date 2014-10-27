7ac0e83e8a9e03bae9f08c71b60d1ea66bdf7019
mprizmic <mprizmic@gmail.com>
2014-10-27 16:22:04 -0300

    test agregado (en proceso)

--- src/Fd/OfertaEducativaBundle/Tests/Model/CarreraManagerTest.php
+++ src/Fd/OfertaEducativaBundle/Tests/Model/CarreraManagerTest.php
@@ -17,15 +17,86 @@ class CarreraManagerTest extends WebTestCase {
                 ->get('ofertaeducativa.carrera.manager');
     }
 
-    public function actualizar(Carrera $entity, Carrera $anterior, $originalOrientaciones = null, $originalTitulos = null) {
+    /**
+     * Para veificar que actualiza se modifica un campo y se lo campara con el valor original
+     * 
+     * @param \Fd\OfertaEducativaBundle\Entity\Carrera $entity
+     * @param \Fd\OfertaEducativaBundle\Entity\Carrera $anterior
+     * @param type $originalOrientaciones
+     * @param type $originalTitulos
+     */
+    public function testActualizar() {
+
+        $parametro_no_usado = $this->manager->crearNuevo();
+        
+        //se prueba con profesorado de primario
+        $carrera_original = $this->manager
+                ->getRepository()
+                ->find(1);
+
+        //se guarda el nombre original para contrastar con el nuevo
+        $nombre_original = $carrera_original->getNombre();
+
+        //se cambia el datos de la carrera
+        $carrera_original->setNombre('xx');
+
+        //se cambia el nombre
+        $this->manager->actualizar($carrera_original, $parametro_no_usado, array(), array());
+
+        $actualizado = $this->manager
+                ->getRepository()
+                ->find(1);
+        
+        //se verifica el cambio
+        $this->assertTrue($actualizado->getNombre() <> $nombre_original);
+        
+        //se vuelve al estado inicial
+        $carrera_original->setNombre($nombre_original);
+        
+        //se cambia el nombre
+        $this->manager->actualizar($carrera_original, $parametro_no_usado, array(), array());
+        
+        
         
     }
 
     public function accept(AsignarVisitadorInterface $visitador) {
-        
+        //ya esta probado en el m{etodo asignarEstablecimientoAction de este test
     }
+    /**
+     * Asigna una carrera a un establecimiento pero queda asignado
+     */
 
-    public function asignarEstablecimiento($carrera_id, $establecimiento_id, $accion) {
+    public function testAsignarEstablecimiento() {
+
+        //se prueba con profesorado de primaria
+        $carrera_id = 1;
+        $repository = $this->manager->getEm()->getRepository('EstablecimientoBundle:Establecimiento');
+
+        //se calcula sobre el ISPEE que se sabe que no tiene profesorado de primaria
+        $establecimiento = $repository->findOneBy(array('apodo' => 'ISPEE'));
+
+        //calcula cuantos establecimientos tienen asignada una carrera
+        $cantidad_anterior = count($repository->findCarreras($establecimiento));
+
+        //hace una asignacion
+        $this->manager->asignarEstablecimiento($carrera_id, $establecimiento->getId(), 'Asignar');
+
+        //recalcula cuantos establecimientos tienen asignada la carrera
+        $carreras = $repository->findCarreras($establecimiento);
+
+        //compara los resultados
+        $this->assertTrue($cantidad_anterior <> count($carreras));
+        
+        //se elimina la asignacion
+        $this->manager->asignarEstablecimiento($carrera_id, $establecimiento->getId(), 'Desasignar');
+        
+        //recalcula cuantos establecimientos tienen asignada la carrera
+        $carreras = $repository->findCarreras($establecimiento);
+
+        //compara los resultados
+        $this->assertTrue($cantidad_anterior == count($carreras));
+        
         
     }
 
@@ -62,7 +133,7 @@ class CarreraManagerTest extends WebTestCase {
                 ->getRepository('OfertaEducativaBundle:Carrera')
                 ->find($id);
 
-        //se veirfica que el objeto fue eliminado
+        //se verifica que el objeto fue eliminado
         $this->assertTrue(is_null($object));
     }
 
@@ -73,16 +144,17 @@ class CarreraManagerTest extends WebTestCase {
         $this->assertTrue($object instanceof Carrera);
     }
 
-    public function desvincular_norma($carrera, $norma) {
+    public function desvincular_norma($carrera = 1, $norma = 10) {
+        //HASTA ACA
         
     }
 
     public function vincular_norma($carrera, $norma) {
-        
+        //HASTA aca
     }
 
     public function eliminar(Carrera $carrera, $flush = false) {
-        
+        //está testeado en el método crearAction de este test
     }
 
     public function testGenerar_combo_json() {
