97e1b97f7a604f1f766cbe79334a47afe0808d3f
mprizmic <mprizmic@gmail.com>
2014-11-02 22:51:35 -0300

    arreglando el merge

--- src/Fd/BackendBundle/Tests/Controller/UnidadOfertaControllerTest.php
+++ src/Fd/BackendBundle/Tests/Controller/UnidadOfertaControllerTest.php
@@ -10,53 +10,6 @@ use Fd\EstablecimientoBundle\Tests\Controller\LoginWebTestCase;
  * FALTA create, update, delete, deleteForm, generarDatosBusquedaPaginada
  */
 class UnidadOfertaControllerTest extends LoginWebTestCase {
-//
-//    public function testBuscar() {
-//        // Create a new client to browse the application
-//        $client = $this->client;
-//
-//        // buscar una autoridad
-//        $crawler = $client->request('GET', '/backend/autoridad/buscar');
-//        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-//
-//        //cliqueo en buscar
-//        $form = $crawler->selectButton('Buscar')->form();
-//        $crawler = $client->submit($form);
-//
-//        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-//
-//        $this->assertGreaterThan(0, $crawler->filter('th:contains("Acciones")')->count()
-//        );
-//
-//        //hace click en la acción editar
-//        $crawler = $client->click($crawler->selectLink('Editar')->link());
-//        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-//
-//        //verifica que fue a la página de edición
-//        $this->assertgreaterThan(0, $crawler->filter('h1:contains("Editar Autoridad")')->count());
-//    }
-//
-//    /** @dataProvider provideUrls */
-//    public function testPageIsSuccessful($url) {
-//        $client = $this->client;
-//        $client->request('GET', $url);
-//        $this->assertTrue($client->getResponse()->isSuccessful());
-//    }
-//
-//    /**
-//     * @return type
-//     */
-//    public function provideUrls() {
-//        $x = '/backend/autoridad';
-//        return array(
-//            array($x . '/buscar'),
-//            array($x . '/1/show'),
-//            array($x . '/new'),
-//            array($x . '/1/edit'),
-//// ...
-//        );
-//    }
-=======
 
     const TER_JOAQUIN = 109;
     const OFERTA_PNP = 13;
@@ -161,6 +114,5 @@ class UnidadOfertaControllerTest extends LoginWebTestCase {
         
         $this->assertTrue( $crawler->filter('a:contains("Agregar un turno")')->count() > 0 );
     }
->>>>>>> b95f24bec1af72ee20ceb5a147074f9fcea685db
 
 }
