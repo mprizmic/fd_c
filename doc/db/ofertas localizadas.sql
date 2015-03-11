48b43da69a42e8d804eb131460248380d5a99359
mprizmic <mprizmic@gmail.com>
2015-03-11 18:32:06 -0300

    se mejora la consulta

--- doc/db/ofertas localizadas.sql
+++ doc/db/ofertas localizadas.sql
@@ -1,11 +1,12 @@
 --- oferta de una unidad educativa sin localizacion
 select e.apodo, e.id as e_id
+,ee.cue_anexo
 ,ee.id as ee_id
 ,uo.id as uo_id
 ,uo.localizacion_id as localizacion_id
 ,oe.id as oe_id
 ,oe.nivel_id oe_nivel_id
-,car.nombre
+,car.nombre, car.id as car_id
 ,uot.id as uot_id
 from Fd.establecimiento e 
 inner join Fd.establecimiento_edificio ee on ee.establecimientos_id=e.id
@@ -14,5 +15,5 @@ inner join Fd.unidad_oferta uo on uo.localizacion_id=l.id
 inner join Fd.oferta_educativa oe on oe.id=uo.oferta_educativa_id
 left join Fd.unidadoferta_turno uot on uo.id=uot.unidad_oferta_id
 left join Fd.carrera car on car.oferta_educativa_id=oe.id
-where e.apodo='ENS 3'
+where e.apodo='ENS 8'
 order by ee.id, oe.nivel_id
