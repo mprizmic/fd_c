1c01c267d923bb950c9f5eb679ec61fcef8125b7
mprizmic <mprizmic@gmail.com>
2015-05-04 19:34:22 -0300

    avance en el comando de migracion

--- src/Fd/EstablecimientoBundle/Command/MigrarLocalizacionCommand.php
+++ src/Fd/EstablecimientoBundle/Command/MigrarLocalizacionCommand.php
@@ -5,9 +5,9 @@
  * OJO está para correr sólo para la ENS 3
  * 
  * Copiar datos de unidad_educativa a localizacion:
- * para cada unidad_oferta se debe tomar la unidad_educativa, ver todas sus localizaciones 
+ * Para cada unidad_oferta se debe tomar la unidad_educativa, ver las localizaciones 00
  * y copiar la localizacion en el lugar de la unidad_educativa.
- * Si hay mas de una localizacion se deben generar nuevos registros en unidad_oferta.
+ * En los casos de los anexos habra que corregir a mano
  * 
  * Este comando se corre para que las ofertas en vez de estar asociadas a una unidad educativa estén asociadas al lugar en donde se dictan.
  * Esto va a permitir hacer la matricula por sede y los titulos por sedes.
@@ -27,77 +27,89 @@ use Fd\EstablecimientoBundle\Entity\Localizacion;
 class MigrarLocalizacionCommand extends ContainerAwareCommand {
 
     var $em;
+    var $logger;
 
     protected function configure() {
         $this
                 ->setName('fd:migrar_localizacion')
                 ->setDescription('Pasa las ofertas de las unidades educativas a la localizacion de las unidades educativas.')
-//            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
-//            ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $logger = $this->getContainer()->get('logger');
 
-        //el container esta disponible con $this->getContainer()->get('ladybug');
+//el container esta disponible con $this->getContainer()->get('ladybug');
         $this->em = $this->getContainer()->get('doctrine.orm.entity_manager');
 
-        //leo todas las unidades educativas de nivel terciario
-        //luego proceso cada una de las unidades educativas
-        //de cada unidad educativa tomo todas sus localizaciones
-        //luego busco todas las unidad_oferta que sean de la unidad educativa en proceso y le cargo la localizacion
-        //si hay mas de una localizacion, entonces genero duplicado de todos los registros de unidad_oferta para las localizaciones a partir de la segunda
+//leo todas las unidades educativas de nivel terciario
+//luego proceso cada una de las unidades educativas
+//de cada unidad educativa tomo su localizacion 00
+//luego busco todas las unidad_oferta que sean de la unidad educativa en proceso y le cargo la localizacion
+//si hay mas de una localizacion, quiere decir que hay un anexo. Esto lo voy a resolver a mano
 
-        $unidades_educativas = $this->em->getRepository('EstablecimientoBundle:UnidadEducativa')->findBy(array('nivel' => 32), array('id' => 'asc'));
+        $unidades_educativas = $this->em->getRepository('EstablecimientoBundle:UnidadEducativa')->findAll();
 
         foreach ($unidades_educativas as $unidad_educativa) {
+            $output->writeln('**********************************************');
             $output->writeln('Unidad_educativa_id: ' . $unidad_educativa->getId());
+            $output->writeln('Nivel: ' . $unidad_educativa->getNivel()->getAbreviatura() . ' del: ' . $unidad_educativa->getEstablecimiento());
 
-            //de cada unidad educativa tomo todas sus localizaciones
+            //de cada unidad educativa tomo todas sus localizaciones y me quedo con cue_anexo = 00
             $localizaciones = $unidad_educativa->getLocalizaciones();
+            $sede = null;
+            foreach ($localizaciones as $localizacion) {
+                if ($localizacion->getEstablecimientoEdificio()->isSede()) {
+                    $sede = $localizacion;
+                    break;
+                }
+            }
 
-            //luego busco todas las unidad_oferta que sean de la unidad educativa en proceso y le cargo la localizacion
-            $unidades_ofertas = $this->em->getRepository('EstablecimientoBundle:UnidadOferta')->findBy(array('unidades' => $unidad_educativa->getId()), array('id' => 'asc'));
-
-            //se copia la localizacion en cada una de las unidades oferta
-            foreach ($unidades_ofertas as $unidad_oferta) {
-
-                $output->writeln('Unidad_oferta_id: ' . $unidad_oferta->getId());
-
-                foreach ($localizaciones as $key => $localizacion) {
-                    if ($key <> 0) {
-
-                        //duplico el registro
-                        $duplicado = new UnidadOferta();
-                        $duplicado->setUnidades($unidad_oferta->getUnidades());
-                        $duplicado->setOfertas($unidad_oferta->getOfertas());
-
-                        //puede no tener turnos o no ser los mismos para todas las localizaciones
-//                        if (!is_null($unidad_oferta->getTurnos())) {
-//                            $duplicado->setTurnos($unidad_oferta->getTurnos());
-//                        }
-
-                        $output->writeln('duplicado creado');
-                        $output->writeln('asigno localizacion' . $localizacion->getId());
-
-                        $this->asignar_localizacion($duplicado, $localizacion);
-                    } else {
-                        $this->asignar_localizacion($unidad_oferta, $localizacion);
-                        $output->writeln('asigno localizacion ' . $localizacion->getId());
-                    }
-                };
-            };
+            //Si tiene hecha la asignacion a la sede continua y sino salta al proximo
+            if ($sede) {
+//                $output->writeln('Localizacion: ' . $sede);
+//
+////            luego busco todas las unidad_oferta que sean de la unidad educativa en proceso y le cargo la localizacion
+//                $unidades_ofertas = $this->em
+//                        ->getRepository('EstablecimientoBundle:UnidadOferta')
+//                        ->findBy(array(
+//                    'unidades' => $unidad_educativa->getId(),
+//                        ), array(
+//                    'id' => 'asc',
+//                        ))
+//                ;
+//
+//                //se copia la localizacion en cada una de las unidades oferta
+//                foreach ($unidades_ofertas as $unidad_oferta) {
+//
+//                    $output->writeln('Unidad_oferta_id: ' . $unidad_oferta->getId());
+//
+//                    $this->asignar_localizacion($unidad_educativa, $sede, $output);
+//                    $output->writeln('asigno localizacion ' . $localizacion->getId());
+//                }
+                
+                
+                
+            
+                    
+                    
+                    
+                    
+            } else {
+                $output->writeln('Localizacion: NO TIENE CARGADA');
+            }
         }
-
-        //salida de pantalla
+//        //salida de pantalla
+        $output->writeln('//////////////////////////////////////////////');
+        $output->writeln('//////////////////////////////////////////////');
         $output->writeln('Proceso todo');
     }
 
-    protected function asignar_localizacion(UnidadOferta $unidad_oferta, Localizacion $localizacion) {
-        $unidad_oferta->setLocalizacion($localizacion);
-        $this->em->persist($unidad_oferta);
-        $this->em->flush();
+    public function asignar_localizacion($unidad_oferta, Localizacion $localizacion, $output) {
+//        $unidad_oferta->setLocalizacion($localizacion);
+//        $this->em->persist($unidad_oferta);
+//        $this->em->flush();
+//        $output->writeln('GRABO: ' . $unidad_oferta . ' - ' . $localizacion);
     }
 
 }
