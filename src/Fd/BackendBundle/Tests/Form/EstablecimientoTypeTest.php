000a182a11c440f1e3e4572f895f992102e30e42
mprizmic <mprizmic@gmail.com>
2015-05-14 18:50:52 -0300

    test del form de establecimiento con sus extensiones

--- src/Fd/BackendBundle/Tests/Form/EstablecimientoTypeTest.php
+++ src/Fd/BackendBundle/Tests/Form/EstablecimientoTypeTest.php
@@ -1,28 +1,57 @@
 <?php
+
 namespace Fd\BackendBundle\Tests\Form;
 
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeExtensionInterface;
 use Symfony\Component\Form\Tests\Extension\Core\Type\TypeTestCase;
 use Fd\BackendBundle\Form\EstablecimientoType;
 use Fd\EstablecimientoBundle\Entity\Establecimiento;
+use Fd\OfertaEducativaBundle\Form\Extension\HelpTypeExtension;
 
-use Acme\TestBundle\Model\TestObject;
+class EstablecimientoTypeTest extends TypeTestCase {
 
-class EstablecimientoTypeTest extends TypeTestCase
-{
-    public function testBindValidData()
-    {
-        $formData = array(
-            'apodo' => 'ENS 2',
-        );
+    protected function setUp() {
+        parent::setUp();
+
+        $this->factory = Forms::createFormFactoryBuilder()
+                ->addTypeExtension(new HelpTypeExtension()
+                )
+                ->addTypeGuesser(
+                        $this->getMockBuilder(
+                                'Symfony\Component\Form\Extension\Validator\ValidatorTypeGuesser'
+                        )
+                        ->disableOriginalConstructor()
+                        ->getMock()
+                )
+                ->getFormFactory();
+
+        $this->dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        $this->builder = new FormBuilder(null, null, $this->dispatcher, $this->factory);
+    }
+
+    /**
+     * @dataProvider getValidTestData
+     */
+    public function testForm($formData) {
+
+        $this->factory->addType(new \Fd\EstablecimientoBundle\Form\Type\SiNoSdType(array(
+            'si' => 'Si',
+            'no' => 'No',
+            'sd' => 'Sin datos',
+        )));
 
         $type = new EstablecimientoType();
         $form = $this->factory->create($type);
 
-        $object = new Establecimiento();
-        $object->fromArray($formData);
-
+        $object = new Establecimiento($formData);
+////        $object->fromArray($formData);
+//
         $form->bind($formData);
-
+//
         $this->assertTrue($form->isSynchronized());
         $this->assertEquals($object, $form->getData());
 
@@ -33,4 +62,15 @@ class EstablecimientoTypeTest extends TypeTestCase
             $this->assertArrayHasKey($key, $children);
         }
     }
+
+    public function getValidTestData() {
+        return array(
+            array(
+                'data' => array(
+                    'apodo' => null,
+                ),
+            ),
+        );
+    }
+
 }
