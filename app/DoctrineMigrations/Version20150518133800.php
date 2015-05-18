07be4d45e48abf2b01afea7a1452effc97e8e00f
mprizmic <mprizmic@gmail.com>
2015-05-18 13:43:25 -0300

    se elimina carrera_estado_validez

--- app/DoctrineMigrations/Version20150518133800.php
+++ app/DoctrineMigrations/Version20150518133800.php
@@ -8,23 +8,16 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150517214010 extends AbstractMigration
-{
-    public function up(Schema $schema)
-    {
+class Version20150518133800 extends AbstractMigration {
+
+    public function up(Schema $schema) {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE establecimiento DROP inscripcion_2_cuatrimestre');
-        $this->addSql('ALTER TABLE carrera ADD comentario VARCHAR(250) DEFAULT NULL');
+
+        $this->addSql('DROP TABLE carrera_estado_validez');
     }
 
-    public function down(Schema $schema)
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('ALTER TABLE carrera DROP comentario');
-        $this->addSql('ALTER TABLE establecimiento ADD inscripcion_2_cuatrimestre VARCHAR(2) DEFAULT NULL');
+    public function down(Schema $schema) {
     }
+
 }
