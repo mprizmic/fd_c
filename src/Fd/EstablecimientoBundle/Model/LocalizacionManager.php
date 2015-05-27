e166744a6201fabd1a865348ecc5d168cda26747
mprizmic <mprizmic@gmail.com>
2015-05-26 23:55:04 -0300

    se pasa la asignacion de domicilios del repository al manager

--- src/Fd/EstablecimientoBundle/Model/LocalizacionManager.php
+++ src/Fd/EstablecimientoBundle/Model/LocalizacionManager.php
@@ -123,4 +123,83 @@ class LocalizacionManager  {
 
         return $this->respuesta;
     }
+
+    /**
+     * Asigna domicilios a una localizacion creando los registros de la tabla domicilio_localizacion
+     * 
+     * @param $entity es la localizacion
+     * @param $data son los datos tal cual vienen en la matriz que se edita en el formulario de la página
+     */
+    public function asignar_domicilios($entity, $data) {
+        /**
+         * estructura de data:
+         * flag: si se marcó o no
+         * nombre
+         * domicilio_id
+         * domicilio_localizacion_id
+         * principal
+         * 
+         */
+        $respuesta = new Respuesta();
+
+        //repositorio de la tabla que se actualiza
+        $repo_dl = $this->em->getRepository('EdificioBundle:DomicilioLocalizacion');
+
+        /**
+         * Si es principal no se toca.
+         * Si no es principal
+         *      Si el flag esta marcado
+         *          se verifica si existe domicilio_localizacion. Se crea si no existe.
+         *      Si el flag no está marcado
+         *          e verifica si existe domcilio_localizacion. Se borra si existe.
+         */
+        $domicilios = $data['domicilios'];
+        foreach ($domicilios as $key => $domicilio) {
+
+//            $repo_dl = $em->getRepository('EdificioBundle:DomicilioLocalizacion');
+
+            if ($domicilio['principal']) {
+                //no se toca
+            } else {
+                $domicilio_localizacion = $repo_dl->find($domicilio['domicilio_localizacion_id']);
+
+                //si el domicilio fue seleccionado, en caso que no exista se crea.
+                if ($domicilio['flag']) {
+
+                    if (!$domicilio_localizacion) {
+                        //no existe y se crea
+                        $domicilio_bd = $em->getRepository('EdificioBundle:Domicilio')
+                                ->find($domicilio['domicilio_id']);
+
+                        $respuesta = $repo_dl->crear($entity, $domicilio_bd);
+                        //ante la primera falla en la grabación se corta el proceso
+                        if ($respuesta->getCodigo() != 1) {
+                            //se corta el proceso porque hubo un error al grabar algún dato
+                            break;
+                        };
+                    }
+                } else {
+                    if ($domicilio_localizacion) {
+                        $domicilio_bd = $this->em->getRepository('EdificioBundle:Domicilio')
+                                ->find($domicilio['domicilio_id']);
+
+                        $respuesta = $repo_dl->eliminar($domicilio_localizacion);
+                        //ante la primera falla en la grabación se corta el proceso
+                        if ($respuesta->getCodigo() != 1) {
+                            //se corta el proceso porque hubo un error al grabar algún dato
+                            break;
+                        };
+                    }
+                }
+            }
+        }
+
+        //si la última grabación sucedió ok todo se grabó ok. Se manda mensaje
+        if ($respuesta->setCodigo(1)) {
+            $respuesta->getMensaje('Se asignaron exitosamente los domicilios.');
+        };
+
+        return $respuesta;
+    }
+    
 }
