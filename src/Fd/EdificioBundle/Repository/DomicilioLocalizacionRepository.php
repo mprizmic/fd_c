94e4c7f1d1da4d5a083cdf0e52ff600d645b8671
mprizmic <mprizmic@gmail.com>
2015-05-27 15:38:19 -0300

    se derivan acciones del repository al manager nuevo

--- src/Fd/EdificioBundle/Repository/DomicilioLocalizacionRepository.php
+++ src/Fd/EdificioBundle/Repository/DomicilioLocalizacionRepository.php
@@ -33,49 +33,6 @@ class DomicilioLocalizacionRepository extends EntityRepository {
         $builder->setParameter($clave, $campo[$clave]);
         return $builder;
     }
-    
-    
-
-    public function crear(Localizacion $localizacion, Domicilio $domicilio) {
-
-        $respuesta = new Respuesta();
-        $em = $this->_em;
-
-        try {
-            $dl = new DomicilioLocalizacion();
-            $dl->setDomicilio($domicilio);
-            $dl->setLocalizacion($localizacion);
-            $em->persist($dl);
-            $em->flush();
-
-            $respuesta->setClaveNueva($dl->getId());
-
-            $respuesta->setCodigo(1);
-            $respuesta->setMensaje('Se guardó la relación entre la localización y el domicilio exitosamente');
-        } catch (Exception $e) {
-            $respuesta->setCodigo(2);
-            $respuesta->setMensaje('No se pudo guardar la relación entre la localización y el domicilio. Verifique los datos y reintente');
-        }
-
-        return $respuesta;
-    }
-
-    public function eliminar(DomicilioLocalizacion $entity) {
-        $respuesta = new Respuesta();
-        $em = $this->_em;
-        try {
-            $em->remove($entity);
-            $em->flush();
-
-            $respuesta->setCodigo(1);
-            $respuesta->setMensaje('Se eliminó la relación entre la localización y los domicilios exitosamente');
-        } catch (Exception $e) {
-            $respuesta->setCodigo(2);
-            $respuesta->setMensaje('No se pudo guardar la relación entre la localización y los domicilios. Verifique los datos y reintente');
-        }
-
-        return $respuesta;
-    }
 
     /**
      * Verifica si en el grupo de domicilios de la localizacion que se esta pasando como parametro, 
@@ -105,39 +62,4 @@ class DomicilioLocalizacionRepository extends EntityRepository {
         };
         return $otro_principal;
     }
-
-    /**
-     * Cambia el domicilio a predeterminado
-     * 
-     * @param type $domicilio_localizacion
-     */
-    public function establecerPredeterminado($domicilio_localizacion) {
-        $valor = TRUE;
-        $this->cambiarPredeterminado($domicilio_localizacion, $valor);
-        return;
-    }
-    /**
-     * Cambia el domicilio a no predeterminado
-     * 
-     * @param type $domicilio_localizacion
-     * @return type
-     */
-    public function cancelarPredeterminado( $domicilio_localizacion ){
-        $valor = FALSE;
-        $this->cambiarPredeterminado($domicilio_localizacion, $valor);
-        return;
-    }
-    /**
-     * Cambia el estado del campo principal de la entidad pasada como parametro
-     * 
-     * @param type $entity
-     * @param type $valor
-     */
-    private function cambiarPredeterminado($entity, $valor) {
-        $entity->setPrincipal($valor);
-        $this->_em->persist($entity);
-        $this->_em->flush();
-        return;
-    }
-
 }
