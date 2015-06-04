ef3f0b50672bfa7d6ad8bfd10f71aa958a7a5d33
mprizmic <mprizmic@gmail.com>
2015-06-04 19:19:50 -0300

    se avanza con el carreaunidadofertahandler.
    se agrega CREAR y ELIMINARALL

--- src/Fd/EstablecimientoBundle/Model/CarreraUnidadOfertaHandler.php
+++ src/Fd/EstablecimientoBundle/Model/CarreraUnidadOfertaHandler.php
@@ -13,4 +13,47 @@ use Fd\OfertaEducativaBundle\Entity\OfertaEducativa;
 
 class CarreraUnidadOfertaHandler extends UnidadOfertaHandler {
 
+    /**
+     * Crea un registro de unidad_oferta
+     * tiene funcionamiento diferente al create del Backend.
+     * Aquí recibe los parámetros para su creación
+     * 
+     * @param type $oferta
+     * @param type $unidad
+     */
+    public function crear($localizacion, $oferta_educativa, $tipo) {
+        return parent::crear($localizacion, $oferta_educativa, 'carrera');
+    }
+
+    /**
+     * Elimina todas las carreras que se estén impartiendo en un edificio de un establecimiento
+     * 
+     * @param Localizacion $localizacion
+     * @param type $flush
+     * @return type
+     */
+    public function eliminarAll(Localizacion $localizacion, $flush = true) {
+
+        //recupero todas las carreras y las especializaciones
+        $ofertas = $localizacion->getOfertas();
+
+        try {
+
+            foreach ($ofertas as $key => $value) {
+                if ($oferta->getTipo() == 'carrera') {
+                    $this->getEm()->remove($entity);
+                }
+            }
+            if ($flush) {
+                $this->getEm()->flush();
+            };
+            $respuesta->setCodigo(1);
+            $respuesta->setMensaje('Se eliminaron las carreras para el edificio seleccionado.');
+        } catch (Exception $ex) {
+            $respuesta->setCodigo(2);
+            $respuesta->setMensaje('No se pudieron eliminar las carreras. Verifíquelo y reintente.');
+        };
+        return $respuesta;
+    }
+
 }
