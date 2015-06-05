dc649ac2c71dc3deacad6b986a139ca8d68aa67c
mprizmic <mprizmic@gmail.com>
2015-06-05 18:01:57 -0300

    se arregla el query

--- doc/db/ofertas de todos los niveles.sql
+++ doc/db/ofertas de todos los niveles.sql
@@ -2,6 +2,7 @@
 select e.apodo, e.id as e_id, 
 ue.id as unidad_educativa
 ,uo.id as uo_id
+, uo.tipo
 ,n.nombre
 ,l.id as l_id
 from Fd.establecimiento e 
@@ -10,5 +11,5 @@ inner join Fd.nivel n on ue.nivel_id=n.id
 INNER JOIN Fd.localizacion l on l.unidad_educativa_id=ue.id
 left join Fd.unidad_oferta uo on uo.localizacion_id=l.id
 left join Fd.oferta_educativa oe on oe.id=uo.oferta_educativa_id
-where e.apodo="Romero"
+where e.apodo="ENS 1"
 order by e.apodo
