5f8849cfdcd81d8ed2cd007590b1f0b3cf201477
mprizmic <mprizmic@gmail.com>
2015-06-05 13:27:55 -0300

    se arreglan las consultas

--- doc/db/ofertas de todos los niveles.sql
+++ doc/db/ofertas de todos los niveles.sql
@@ -2,15 +2,13 @@
 select e.apodo, e.id as e_id, 
 ue.id as unidad_educativa
 ,uo.id as uo_id
-,uo.localizacion_id as localizacion_id
 ,n.nombre
-,car.nombre
-,car.oferta_educativa_id
+,l.id as l_id
 from Fd.establecimiento e 
 inner join Fd.unidad_educativa ue on ue.establecimiento_id= e.id
 inner join Fd.nivel n on ue.nivel_id=n.id
-inner join Fd.unidad_oferta uo on uo.unidad_educativa_id=ue.id
-inner join Fd.oferta_educativa oe on oe.id=uo.oferta_educativa_id
-inner join Fd.carrera car on car.oferta_educativa_id=oe.id
-where ue.id=103
+INNER JOIN Fd.localizacion l on l.unidad_educativa_id=ue.id
+left join Fd.unidad_oferta uo on uo.localizacion_id=l.id
+left join Fd.oferta_educativa oe on oe.id=uo.oferta_educativa_id
+where e.apodo="Romero"
 order by e.apodo
