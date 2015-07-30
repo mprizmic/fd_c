dbfebdd8bd3bc22e1f245b7b7cec8ceda15fc96d
mprizmic <mprizmic@gmail.com>
2015-07-30 00:40:54 -0300

    arreglos de estetica

--- src/Fd/BackendBundle/Form/RecursoType.php
+++ src/Fd/BackendBundle/Form/RecursoType.php
@@ -6,26 +6,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class RecursoType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class RecursoType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('codigo')
-            ->add('descripcion')
-            ->add('orden')
+                ->add('codigo', null, array(
+                    'label' => 'Código',
+                ))
+                ->add('descripcion', null, array(
+                    'label' => 'Descripción',
+                ))
+                ->add('orden')
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Fd\TablaBundle\Entity\Recurso'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'fd_tablabundle_recursotype';
     }
+
 }
