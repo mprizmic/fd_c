f377b87b6cb20e817db57fe59fdb4682632b9c4b
mprizmic <mprizmic@gmail.com>
2015-08-03 17:21:07 -0300

    queries

--- doc/db/ofertas localizadas.sql
+++ doc/db/ofertas localizadas.sql
@@ -20,5 +20,5 @@ left join Fd.unidadoferta_turno uot on uo.id=uot.unidad_oferta_id
 left join Fd.carrera car on car.oferta_educativa_id=oe.id
 left join Fd.secundario sec on sec.oferta_educativa_id=oe.id
 left join Fd.turno tur on tur.id=uot.turno_id
-where n.abreviatura ="Med"
+where n.abreviatura ="Ter"
 order by e.apodo, ee.cue_anexo, oe.nivel_id
