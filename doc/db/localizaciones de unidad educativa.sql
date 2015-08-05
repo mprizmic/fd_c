df00091c43d0b1143f84adc3ee43d6316bc61532
mprizmic <mprizmic@gmail.com>
2015-08-05 17:47:50 -0300

    se mejora el query

--- doc/db/localizaciones de unidad educativa.sql
+++ doc/db/localizaciones de unidad educativa.sql
@@ -2,14 +2,16 @@
 
 select e.apodo, e.id as e_id, 
 ue.id as unidad_educativa
+,ee.id as ee_id
 ,l.id as localizacion_id
 ,d.calle , d.altura
 from Fd.establecimiento e 
+inner join Fd.establecimiento_edificio ee on ee.establecimientos_id=e.id
 inner join Fd.unidad_educativa ue on ue.establecimiento_id= e.id
 inner join Fd.nivel n on ue.nivel_id=n.id
-inner join Fd.localizacion l on l.unidad_educativa_id=ue.id
+inner join Fd.localizacion l on l.unidad_educativa_id=ue.id and l.establecimiento_edificio_id=ee.id
 inner join Fd.domicilio_localizacion dl on dl.localizacion_id=l.id
 inner join Fd.domicilio d on d.id=dl.domicilio_id
-where e.apodo="Romero"
+where e.apodo="JBJusto"
 order by e.apodo
 
