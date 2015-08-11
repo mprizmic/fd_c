89e3c1d1c73da338c37d47ff8b57318482e17bf0
mprizmic <mprizmic@gmail.com>
2015-08-11 17:55:22 -0300

    se elimina de la tabla establecimeinto el te y el mail y se pasa a la tabla establecimientoedificio

---
+++ app/DoctrineMigrations/Version20150811173756.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150811173756 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE establecimiento DROP email, DROP te");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE establecimiento ADD email VARCHAR(255) DEFAULT NULL, ADD te VARCHAR(25) DEFAULT NULL");
+    }
+}
