3d3b801bea639738f89abc6689c7755a0d4a0b89
mprizmic <mprizmic@gmail.com>
2015-08-28 17:11:34 -0300

    se mejora el crud de las salas de inicial

--- src/Fd/OfertaEducativaBundle/Form/InicialXType.php
+++ src/Fd/OfertaEducativaBundle/Form/InicialXType.php
@@ -5,7 +5,10 @@ namespace Fd\OfertaEducativaBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 use Fd\OfertaEducativaBundle\Entity\Sala;
 use Fd\OfertaEducativaBundle\Form\SalaType;
 use Fd\OfertaEducativaBundle\Entity\InicialX;
@@ -14,6 +17,30 @@ use Fd\TablaBundle\Entity\GrupoEtario;
 class InicialXType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
+        
+//        $factory = $builder->getFormFactory();
+//        /*
+//         * si el rgistro ya está creado no se pueden cambiar la unidad_oferta
+//         * si es un registro nuevo el campo estàn disponibles para cargarlos 
+//         */
+//        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) use ($factory) {
+//
+//            $form = $event->getForm();
+//            $data = $event->getData();
+//
+//            $optionsUnidadOferta = array(
+//                'class' => 'EstablecimientoBundle:UnidadOferta',
+//                'label'=> 'Oferta Localizada',
+//            );
+//
+//            if ($data->getId()) {
+//                //si el registro ya está creado no se pueden cambiar ni la unidad educativa ni el establecimeinto
+//                $optionsUnidadOferta['disabled'] = true;
+//            };
+//
+//            $form->add(
+//                    $factory->createNamed('unidad_oferta', 'entity', null, $optionsUnidadOferta));
+//        });        
         $builder
                 ->add('matricula', 'integer', array(
                     'required' => false,
