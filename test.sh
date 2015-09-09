9eb2b17af5caf3dbdb98283623e98d4f2e1387fe
mprizmic <mprizmic@gmail.com>
2015-09-09 17:32:44 -0300

    lanza todos los test

---
+++ test.sh
@@ -0,0 +1,16 @@
+phpunit -c app src/Fd/EstablecimientoBundle/Tests/Controller/DefaultControllerTest.php
+phpunit -c app src/Fd/EstablecimientoBundle/Tests/Controller/EstablecimientoControllerTest.php
+phpunit -c app src/Fd/EstablecimientoBundle/Tests/Controller/UnidadEducativaControllerTest.php
+phpunit -c app src/Fd/EstablecimientoBundle/Tests/Controller/UnidadOfertaControllerTest.php
+#
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/AutoridadControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/DomicilioControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/EstablecimientoControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/EstablecimientoEdificioControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/LocalizacionControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/OfertaEducativaControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/PortadaControllerTest.php
+phpunit -c app src/Fd/BackendBundle/Tests/Controller/VecinoControllerTest.php
+
+
+
