bbf2f59cffb23a3cfb359cf1764a76e41a3e88b3
mprizmic <mprizmic@gmail.com>
2015-09-29 18:58:07 -0300

    queries

--- doc/db/ofertas de todos los niveles.sql
+++ doc/db/ofertas de todos los niveles.sql
@@ -1,15 +1,16 @@
 --- oferta de una unidad educativa sin localizacion
-select e.apodo, e.id as e_id, 
-ue.id as unidad_educativa
+select e.apodo, e.id as e_id
+,ue.id as unidad_educativa
 ,uo.id as uo_id
 , uo.tipo
 ,n.nombre
 ,l.id as l_id
+,oe.id as oe_id
 from Fd.establecimiento e 
 inner join Fd.unidad_educativa ue on ue.establecimiento_id= e.id
 inner join Fd.nivel n on ue.nivel_id=n.id
 INNER JOIN Fd.localizacion l on l.unidad_educativa_id=ue.id
 left join Fd.unidad_oferta uo on uo.localizacion_id=l.id
 left join Fd.oferta_educativa oe on oe.id=uo.oferta_educativa_id
-where e.apodo="ENS 1"
+where e.apodo="Joaquin"
 order by e.apodo
--- doc/db/ofertas localizadas.sql
+++ doc/db/ofertas localizadas.sql
@@ -1,13 +1,14 @@
 --- oferta de una unidad educativa sin localizacion
 select e.apodo, e.id as e_id
 ,ee.cue_anexo
+,ee.nombre
 ,uo.id as uo_id
 ,uo.localizacion_id as localizacion_id
 ,uo.tipo
 ,n.abreviatura
 ,oe.id as oe_id
 ,oe.nivel_id oe_nivel_id
--- ,car.nombre, car.id as car_id
+,car.nombre, car.id as car_id
 ,tur.descripcion
 from Fd.establecimiento e 
 inner join Fd.establecimiento_edificio ee on ee.establecimientos_id=e.id
@@ -21,4 +22,5 @@ left join Fd.carrera car on car.oferta_educativa_id=oe.id
 left join Fd.secundario sec on sec.oferta_educativa_id=oe.id
 left join Fd.turno tur on tur.id=uot.turno_id
 where n.abreviatura ="Ter"
-order by e.apodo, ee.cue_anexo, oe.nivel_id
+and e.apodo = 'ENS 3'
+order by e.orden, ee.cue_anexo, oe.nivel_id
