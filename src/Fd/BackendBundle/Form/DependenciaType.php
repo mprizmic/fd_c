579b150c4caddeb8d042e975d625fb186b581d8b
mprizmic <mprizmic@gmail.com>
2015-10-29 16:23:17 -0300

    le agrego turno y nivel a la dependencia

--- src/Fd/BackendBundle/Form/DependenciaType.php
+++ src/Fd/BackendBundle/Form/DependenciaType.php
@@ -9,7 +9,11 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Fd\TablaBundle\Entity\Dependencia;
+use Fd\TablaBundle\Entity\Nivel;
+use Fd\TablaBundle\Entity\Turno;
 use Fd\TablaBundle\Repository\DependenciaRepository;
+use Fd\TablaBundle\Repository\NivelRepository;
+use Fd\TablaBundle\Repository\TurnoRepository;
 
 class DependenciaType extends AbstractType {
 
@@ -22,10 +26,27 @@ class DependenciaType extends AbstractType {
                 ->add('codigo', 'text', array(
                     'trim' => true,
                     'label' => 'Código',
-                    ))
+                ))
                 ->add('orden', 'number')
+                ->add('turno', 'entity', array(
+                    'class' => 'TablaBundle:Turno',
+                    'empty_value' => 'Seleccione un turno ...',
+                    'required' => false,
+                    'query_builder' => function (TurnoRepository $repository) {
+                        return $repository->qbOrdenado();
+                        createQueryBuilder('t');
+                    },
+                ))
+                ->add('nivel', 'entity', array(
+                    'class' => 'TablaBundle:Nivel',
+                    'empty_value' => 'Seleccione un nivel ...',
+                    'required' => false,
+                    'property' => 'nombre',
+                    'query_builder' => function (NivelRepository $repository) {
+                        return $repository->qbOrdenado();
+                    },
+                ))
         ;
-
     }
 
     public function getName() {
