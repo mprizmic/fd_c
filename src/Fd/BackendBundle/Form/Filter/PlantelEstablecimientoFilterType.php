b3b4d6fd0522bd839ba45d1174d5154666bf8c60
mprizmic <mprizmic@gmail.com>
2015-11-09 22:58:25 -0300

    avance y terminacion de los filtros de plantel_Establecimiento

--- src/Fd/BackendBundle/Form/Filter/PlantelEstablecimientoFilterType.php
+++ src/Fd/BackendBundle/Form/Filter/PlantelEstablecimientoFilterType.php
@@ -6,6 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Fd\EstablecimientoBundle\Entity\PlantelEstablecimiento;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query\Expr;
+
+use Lexik\Bundle\FormFilterBundle\Filter\Query\QueryInterface;
 
 class PlantelEstablecimientoFilterType extends AbstractType {
 
@@ -26,6 +30,16 @@ class PlantelEstablecimientoFilterType extends AbstractType {
                     'attr' => array(
                         'class' => 'input_talle_4',
                     ),
+                    'apply_filter' => function (QueryInterface $filterQuery, $field, $values) {
+                            if (empty($values['value'])) {
+                                return null;
+                            };
+                            
+                            $query = $filterQuery->getQueryBuilder();
+                            $query->add('where', $query->expr()->eq('oi.establecimiento',':pe_establecimiento'));
+                            $query->setParameter('pe_establecimiento', $values['value']);
+  
+                        },
                 ))
                 ->add('dependencia', 'filter_choice', array(
                     'label' => 'Dependencias',
@@ -34,6 +48,36 @@ class PlantelEstablecimientoFilterType extends AbstractType {
                     'attr' => array(
                         'class' => 'input_talle_4',
                     ),
+                    'apply_filter'=>function(QueryInterface $filterQuery, $field, $values) {
+                            if (empty($values['value'])) {
+                                return null;
+                            };
+                            
+                            $query = $filterQuery->getQueryBuilder();
+                            
+                            // $query->add('where', $query->expr()->eq('oi.dependencia', ':pe_dependencia'));
+                            $args[0] = $query->expr()->eq('oi.dependencia', ':pe_dependencia');
+                            
+                            //va a testear si ya hay alguna condicion where 
+                            $where = $query->getDQLPart('where');
+                            
+                            if ($where instanceof Expr\Comparison){
+                                
+                                //ya existe condicion where así que la une con esta condicion where
+                                array_unshift($args, $where);
+                                
+                                //convierte el array en un objeto condicion andx
+                                $where = new Expr\Andx($args);
+                            }else{
+                                $where = $args[0];
+                            };
+
+                            //le agrega el where al querybuilder
+                            $query->add('where', $where);
+                            
+                            $query->setParameter('pe_dependencia', $values['value']);
+                            
+                        },
                 ))
                 ->add('cargo', 'filter_choice', array(
                     'label' => 'Cargos',
@@ -42,7 +86,7 @@ class PlantelEstablecimientoFilterType extends AbstractType {
                     'attr' => array(
                         'class' => 'input_talle_4',
                     ),
-                ))
+                 ))
         ;
     }
 
