bcee645631f16a856cb58411cbc1452986383cc3
mprizmic <mprizmic@gmail.com>
2015-11-25 00:29:10 -0300

    avances en la carga del edit de autoridad

--- src/Fd/EstablecimientoBundle/Repository/PlantelEstablecimientoRepository.php
+++ src/Fd/EstablecimientoBundle/Repository/PlantelEstablecimientoRepository.php
@@ -3,6 +3,7 @@
 namespace Fd\EstablecimientoBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Fd\EstablecimientoBundle\Entity\EstablecimientoEdificio;
 use Fd\EstablecimientoBundle\Entity\PlantelEstablecimiento;
 
 class PlantelEstablecimientoRepository extends EntityRepository {
@@ -13,14 +14,14 @@ class PlantelEstablecimientoRepository extends EntityRepository {
 
     public function qbAllOrdenado() {
         return $this->qbAll()
-                ->join('pe.organizacion', 'oi')
-                ->join('oi.establecimiento', 'ee')
-                ->join('ee.establecimientos', 'e')
-                ->join('oi.dependencia', 'd')
-                ->join('pe.cargo', 'cg')
-                ->orderBy('e.orden', 'ASC')
-                ->addOrderBy('d.orden', 'ASC')
-                ->addOrderBy('cg.orden', 'ASC');
+                        ->join('pe.organizacion', 'oi')
+                        ->join('oi.establecimiento', 'ee')
+                        ->join('ee.establecimientos', 'e')
+                        ->join('oi.dependencia', 'd')
+                        ->join('pe.cargo', 'cg')
+                        ->orderBy('e.orden', 'ASC')
+                        ->addOrderBy('d.orden', 'ASC')
+                        ->addOrderBy('cg.orden', 'ASC');
     }
 
     public function qyAll() {
@@ -33,6 +34,12 @@ class PlantelEstablecimientoRepository extends EntityRepository {
 
     public function findAllOrdenado() {
         return $this->qyAllOrdenado()->getResult();
+    }
+
+    public function qbAllByEstablecimiento(EstablecimientoEdificio $establecimiento_edificio) {
+        return $this->qbAllOrdenado()
+                        ->where('ee.id = :establecimiento_edificio')
+                        ->setParameter('establecimiento_edificio', $establecimiento_edificio);
     }
 
 }
