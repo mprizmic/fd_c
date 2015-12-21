1282f6ecd04a97198485f3f05b56e265bae514c3
mprizmic <mprizmic@gmail.com>
2015-12-21 14:26:37 -0300

    se hace un script para la migracion y se asientan el seguimiento

---
+++ app/DoctrineMigrations/Version20151221142318.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 
+ * se corre al migrar la agenda al sistema de producción
+ * 
+ * hace falta eliminar los datos existentes de autoridades 
+ * 
+ */
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151221142318 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("delete from autoridad");
+        $this->addSql("delete from plantel_establecimiento");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
